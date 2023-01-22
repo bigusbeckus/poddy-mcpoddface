b1aefa7bbeda0f73b0ffa82e10135158acc3c4ed
beck <b.tadesse.77@gmail.com>
2023-01-22 14:20:01 +0300

    Clean up theme switcher logic

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,15 @@
 import { Listbox } from "@headlessui/react";
-import React, { useState } from "react";
+import React from "react";
+import { Monitor, Moon, Sun } from "react-feather";
 import { AppThemeMode, useTheme } from "../hooks/theme";
 
 const themes: AppThemeMode[] = ["system", "dark", "light"];
 
 export const DefaultFooter: React.FC = () => {
-  // const [darkMode, setDarkMode] = useContext(ThemeContext);
   const [theme, setTheme] = useTheme();
-  const [selectedTheme, setSelectedTheme] = useState(theme.mode);
 
   const handleThemeSelection = (value: AppThemeMode) => {
     setTheme(value);
-    setSelectedTheme(value);
   };
 
   return (
@@ -25,7 +23,7 @@ export const DefaultFooter: React.FC = () => {
       </div>
       <div className="col-span-2 flex justify-end">
         <div className="w-40 relative">
-          <Listbox value={selectedTheme} onChange={handleThemeSelection}>
+          <Listbox value={theme.mode} onChange={handleThemeSelection}>
             <Listbox.Options className="w-full absolute bottom-10 max-h-96 py-1 rounded-md bg-black/20 dark:bg-white/20 ring-1 ring-black dark:ring-white ring-opacity-30 dark:ring-opacity-30 focus:outline-none text-sm">
               {themes.map((theme) => (
                 <Listbox.Option
@@ -44,9 +42,33 @@ export const DefaultFooter: React.FC = () => {
               ))}
             </Listbox.Options>
             <Listbox.Button className="relative w-full px-3 py-2 rounded-md text-black dark:text-white bg-black/20 dark:bg-white/20">
-              <span className="block truncate text-sm capitalize">
-                {selectedTheme}
-              </span>
+              <div className="flex items-center gap-3">
+                <div>
+                  <Monitor
+                    size={16}
+                    className={`relative left-0 transition-opacity duration-300 ${
+                      theme.mode !== "system" && "opacity-0 h-0"
+                    }`}
+                  />
+                  <Moon
+                    size={16}
+                    className={`relative left-0 transition-opacity duration-300 ${
+                      theme.mode !== "dark" && "opacity-0 h-0"
+                    }`}
+                  />
+                  <Sun
+                    size={16}
+                    className={`relative left-0 transition-opacity duration-300 ${
+                      theme.mode !== "light" && "opacity-0 h-0"
+                    }`}
+                  />
+                </div>
+                <span className="block truncate text-sm capitalize">
+                  {theme.mode}{" "}
+                  {theme.mode !== "system" ||
+                    (theme.dark ? "(dark)" : "(light)")}
+                </span>
+              </div>
             </Listbox.Button>
           </Listbox>
         </div>
