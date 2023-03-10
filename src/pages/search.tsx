f15cdc25b90852a135bb2025a9c57a0bb00a402c
Beck <b.tadesse.77@gmail.com>
2023-03-10 23:48:22 +0300

    Rework search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,17 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { AnimatePresence } from "framer-motion";
 import { AnimatedLayout } from "layouts/animated";
-import { podcastSearchLink } from "libs/itunes-podcast";
+import { podcastSearchLink, SearchReturn } from "libs/itunes-podcast";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
 
 export const getServerSideProps: GetServerSideProps<{
   q: string;
 }> = async ({ query }) => {
   const q = query.q?.toString();
-  console.log(q);
-  console.log(query.query);
 
   if (!q) {
     return {
@@ -29,38 +27,78 @@ export const getServerSideProps: GetServerSideProps<{
   };
 };
 
-const Search = ({ q }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const searchLink = podcastSearchLink().term(q);
-  const { data, error, isLoading } = useQuery(["podcasts"], searchLink.fetch);
+// Search page root
+function SearchPage({ q }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const queryClient = useQueryClient();
 
-  const [searchTerms, setSearchTerms] = useState(q);
+  const queryResult = useQuery({
+    queryKey: ["podcasts", "search", q],
+    queryFn: () => podcastSearchLink().term(q).fetch(),
+    placeholderData: () => {
+      return queryClient.getQueryData<SearchReturn>(["podcasts"]);
+    },
+  });
+
+  return (
+    <AnimatedLayout>
+      <div className="h-full">
+        <AnimatePresence mode="wait" initial={false}>
+          <SearchPageComponentDiscriminator q={q} queryResult={queryResult} />
+        </AnimatePresence>
+      </div>
+    </AnimatedLayout>
+  );
+}
+
+// Loading/error handler and component selector
+function SearchPageComponentDiscriminator({
+  q,
+  queryResult,
+}: {
+  q: string;
+  queryResult: {
+    data: SearchReturn | undefined;
+    error: unknown;
+    isLoading: boolean;
+  };
+}) {
+  const { data, error, isLoading } = queryResult;
 
   if (isLoading) {
-    return (
-      <AnimatedLayout>
-        <div>Loading search results...</div>
-      </AnimatedLayout>
-    );
+    return <SearchPageLoading />;
   }
 
   if (error || !data) {
-    return (
-      <AnimatedLayout>
-        <div> {error instanceof Error ? error.message : `Search for podcast: "${q}" failed`}</div>
-      </AnimatedLayout>
-    );
+    return <SearchPageError error={error} q={q} />;
   }
 
   if (data.resultCount === 0) {
-    return (
-      <AnimatedLayout>
-        <div className="flex h-full items-center justify-center overflow-hidden">
-          <div className="text-3xl font-light">No results found</div>
-        </div>
-      </AnimatedLayout>
-    );
+    return <NoSearchResults />;
   }
 
+  return <SearchResults data={data} q={q} />;
+}
+
+// Loading state
+function SearchPageLoading() {
+  return (
+    <AnimatedLayout>
+      <div>Loading search results...</div>
+    </AnimatedLayout>
+  );
+}
+
+// Error component
+function SearchPageError({ error, q }: { error: unknown; q: string }) {
+  return (
+    <AnimatedLayout>
+      <div> {error instanceof Error ? error.message : `Search for podcast: "${q}" failed`}</div>
+    </AnimatedLayout>
+  );
+}
+
+// Search results display component
+function SearchResults({ data, q }: { data: SearchReturn; q: string }) {
   return (
     <AnimatedLayout>
       <Head>
@@ -68,7 +106,9 @@ const Search = ({ q }: InferGetServerSidePropsType<typeof getServerSideProps>) =
       </Head>
       <div className="h-full overflow-y-scroll p-8">
         <div>
-          <h1 className="text-3xl font-light">{data.resultCount.toString()} result(s) found</h1>
+          <h1 className="text-3xl font-light">
+            {data.resultCount.toString()} result(s) found for &quot;{q}&quot;
+          </h1>
           <hr className="my-8 border-white/10" />
           <ul>
             {data.results.map((podcast) => (
@@ -83,6 +123,20 @@ const Search = ({ q }: InferGetServerSidePropsType<typeof getServerSideProps>) =
       </div>
     </AnimatedLayout>
   );
-};
+}
 
-export default Search;
+// No results component
+function NoSearchResults() {
+  return (
+    <AnimatedLayout>
+      <Head>
+        <title>No results found - Search - Poddy McPodface</title>
+      </Head>
+      <div className="flex h-full items-center justify-center overflow-hidden">
+        <div className="text-3xl font-light">No results found</div>
+      </div>
+    </AnimatedLayout>
+  );
+}
+
+export default SearchPage;
