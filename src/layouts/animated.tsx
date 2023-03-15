28a3d833adc94255e9a928e763138356b519346e
Beck <b.tadesse.77@gmail.com>
2023-03-16 00:47:31 +0300

    Fix minor animated layout issues

--- src/layouts/animated.tsx
+++ src/layouts/animated.tsx
@@ -1,8 +1,35 @@
 import { m } from "framer-motion";
 
-export const AnimatedLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+type AnimatedLayoutProps = {
+  containerType?: "div" | "span";
+  className?: string;
+  children: React.ReactNode;
+};
+
+export const AnimatedLayout: React.FC<AnimatedLayoutProps> = ({
+  children,
+  className,
+  containerType,
+}) => {
+  if (containerType === "div") {
+    return (
+      <m.div
+        className={className}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          duration: 0.15,
+        }}
+      >
+        {children}
+      </m.div>
+    );
+  }
+
   return (
     <m.span
+      className={className}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
