6b97bbd2ef642897c3c3cab362fb5174a947672a
Beck <b.tadesse.77@gmail.com>
2023-03-16 00:52:19 +0300

    Make minor changes to the search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,14 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { TextField } from "@/components/input/text-field";
+import { PodcastThumb } from "@/components/podcast-thumb";
 import { AnimatePresence } from "framer-motion";
-import { AnimatedLayout } from "layouts/animated";
-import { podcastSearchLink, SearchReturn } from "libs/itunes-podcast";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+// import { useDebounce } from "@/hooks/debounce";
+import { AnimatedLayout } from "@/layouts/animated";
+import { podcastSearchLink, type SearchReturn } from "@/libs/itunes-podcast";
+import { type GetServerSideProps, type InferGetServerSidePropsType } from "next";
 import Head from "next/head";
-import Link from "next/link";
+// import Link from "next/link";
+import { type FormEvent, useState, type ChangeEvent } from "react";
 
 export const getServerSideProps: GetServerSideProps<{
   q: string;
@@ -31,20 +35,66 @@ export const getServerSideProps: GetServerSideProps<{
 function SearchPage({ q }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const queryClient = useQueryClient();
 
+  const [input, setInput] = useState(q);
+  const [searchTerms, setSearchTerms] = useState(q);
+
   const queryResult = useQuery({
-    queryKey: ["podcasts", "search", q],
-    queryFn: () => podcastSearchLink().term(q).fetch(),
+    queryKey: ["podcasts", "search", searchTerms],
+    queryFn: () => podcastSearchLink().term(searchTerms).fetch(),
     placeholderData: () => {
       return queryClient.getQueryData<SearchReturn>(["podcasts"]);
     },
   });
 
+  // const debouncedRefetch = useDebounce(queryResult.refetch);
+
+  function handleSearchTermsChange(e: ChangeEvent<HTMLInputElement>) {
+    setInput(e.target.value);
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (input) {
+      const url = encodeURI(window.location.href.split("?")[0] + "?q=" + input);
+      window.history.pushState({ path: url }, "", url);
+      setSearchTerms(input);
+    }
+  }
+
   return (
-    <AnimatedLayout>
-      <div className="h-full">
-        <AnimatePresence mode="wait" initial={false}>
-          <SearchPageComponentDiscriminator q={q} queryResult={queryResult} />
-        </AnimatePresence>
+    <AnimatedLayout containerType="div" className="block h-full">
+      <div className="flex h-full flex-col">
+        <div className="flex justify-center p-2">
+          <form onSubmit={handleSubmit}>
+            <TextField
+              value={input}
+              onChange={handleSearchTermsChange}
+              placeholder="Search"
+              className="w-96 text-xl"
+            />
+          </form>
+        </div>
+        <div className="flex flex-1">
+          <div className="w-96 p-4">
+            <div className="rounded-md bg-gray-900 p-8">
+              <h3 className="text-2xl font-extrabold text-emerald-500">Filter</h3>
+              {/* <hr className="my-4 border-b-0 border-gray-800" /> */}
+              <div className="py-2"></div>
+              <div>
+                <ul>
+                  <li>Option 1</li>
+                  <li>Option 2</li>
+                  <li>Option 3</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex h-full flex-1">
+            <AnimatePresence mode="wait" initial={false}>
+              <SearchPageComponentDiscriminator q={searchTerms} queryResult={queryResult} />
+            </AnimatePresence>
+          </div>
+        </div>
       </div>
     </AnimatedLayout>
   );
@@ -110,15 +160,25 @@ function SearchResults({ data, q }: { data: SearchReturn; q: string }) {
             {data.resultCount.toString()} result(s) found for &quot;{q}&quot;
           </h1>
           <hr className="my-8 border-white/10" />
-          <ul>
+          {/* <ul> */}
+          {/*   {data.results.map((podcast) => ( */}
+          {/*     <li key={podcast.collectionId}> */}
+          {/*       <Link href={`podcast/${podcast.collectionId}`}> */}
+          {/*         <a className="hover:underline">{podcast.collectionName}</a> */}
+          {/*       </Link> */}
+          {/*     </li> */}
+          {/*   ))} */}
+          {/* </ul> */}
+          <div className="grid grid-cols-10 gap-1 pb-4">
             {data.results.map((podcast) => (
-              <li key={podcast.collectionId}>
-                <Link href={`podcast/${podcast.collectionId}`}>
-                  <a className="hover:underline">{podcast.collectionName}</a>
-                </Link>
-              </li>
+              <PodcastThumb
+                key={podcast.collectionId}
+                id={podcast.collectionId}
+                artworkUrl={podcast.artworkUrl600}
+                name={podcast.collectionName}
+              />
             ))}
-          </ul>
+          </div>
         </div>
       </div>
     </AnimatedLayout>
